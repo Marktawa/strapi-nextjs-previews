a369ef839d2dda0d64a0fb72b3f3ebfca76212af
McTawa <markmunyaka@gmail.com>
2022-07-26 13:58:54 +0000

    Updated next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const path = require('path');
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+};
