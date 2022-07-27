71ab23a96b3035869beda81f0e55ee4b722f2e7d
McTawa <markmunyaka@gmail.com>
2022-07-27 11:58:09 +0000

    Strapi v4 compatible

--- frontend/lib/articles.js
+++ frontend/lib/articles.js
@@ -16,9 +16,9 @@ async function fetchArticleApi(slug) {
 
 // Helper function to GET a single article from Strapi which is in draft state
 async function fetchArticlePreviewApi(slug) {
-  const requestUrl = `${STRAPI_URL}/articles?_publicationState=preview&slug=${slug}`;
+  const requestUrl = `${STRAPI_URL}/articles?publicationState=preview&filters[slug][$eq]=${slug}`;
   const response = await fetch(requestUrl);
-  return (await response.json())[0];
+  return await response.json();
 }
 
 export { fetchArticlesApi, fetchArticleApi, fetchArticlePreviewApi };
